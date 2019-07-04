b5f8bac860e891504fb9fbc56c53b148109359dc
ansarimofid <ansarimofid@gmail.com>
2019-07-04 11:03:55 +0530

    Updates

---
+++ precache-manifest.80ca92857c64c95b40fc878d580f51b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5411d07a9feee8e7921cdefccc2219b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2106e59142c0c7ad086",
+    "url": "/static/css/main.45bc7c11.chunk.css"
+  },
+  {
+    "revision": "8bdc6b0e45b99eb456cf",
+    "url": "/static/js/2.b705c5ce.chunk.js"
+  },
+  {
+    "revision": "b2106e59142c0c7ad086",
+    "url": "/static/js/main.bf46de15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a275e7d8291f1b37ef09db4485f79a4",
+    "url": "/static/media/hero-image.7a275e7d.png"
+  },
+  {
+    "revision": "9dbec41c82b0d746148e7b483483ad18",
+    "url": "/static/media/reward-img.9dbec41c.png"
+  },
+  {
+    "revision": "782ded6ea86837e03b92a19c73b47e76",
+    "url": "/static/media/steem-img.782ded6e.png"
+  },
+  {
+    "revision": "45cb0ec60f5d422299037df7549a2329",
+    "url": "/static/media/supp-img.45cb0ec6.png"
+  }
+]);

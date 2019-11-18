b8cbc8daf8dcb3aa7bb7162a6f675f45aca085aa
ansarimofid <ansarimofid@gmail.com>
2019-11-18 15:36:06 +0530

    Adds privacy policy page

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
           <li className="ml-6 mb-4"><a href="https://www.instagram.com/gosocialapp_co/" target="__new">Instagram</a></li>
           <li className="ml-6 mb-4"><a href="https://twitter.com/GoSocialapp_co" target="__new">Twitter</a></li>
           
-          <li className="ml-6 mb-4"><a href="https://www.getgosocial.app/privacy-policy" target="__new">Privacy Policy</a></li>
+          <li className="ml-6 mb-4"><a href="/privacy-policy.html" target="__new">Privacy Policy</a></li>
           
           <li className="ml-6 mb-4"><a href="mailto:gosocial@hapramp.com">gosocial@hapramp.com</a></li>
         </ul>

aad85c3368de55b1eff35d109b63f8fdec9454c6
ansarimofid <ansarimofid@gmail.com>
2019-11-20 20:57:30 +0530

    Updates footer links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -9,11 +9,12 @@ const Footer = () => {
           <div className="ml-2 text-lg">By <a className="border-primary border-b-2 text-primary ml-2" href="https://hapramp.com" target="__new">HapRamp Studio</a></div>
         </div>
         <ul className="flex text-lg mt-8 md:mt-4 flex-wrap -ml-6 md:ml-8">
-          <li className="ml-6 mb-4"><a href="https://www.facebook.com/GoSocialapp.co" target="__new">Facebook</a></li>
-          <li className="ml-6 mb-4"><a href="https://www.instagram.com/gosocialapp_co/" target="__new">Instagram</a></li>
-          <li className="ml-6 mb-4"><a href="https://twitter.com/GoSocialapp_co" target="__new">Twitter</a></li>
+          <li className="ml-6 mb-4"><a href="https://www.facebook.com/getgosocialapp/" target="__new">Facebook</a></li>
+          <li className="ml-6 mb-4"><a href="https://www.instagram.com/getgosocial.app/" target="__new">Instagram</a></li>
+          <li className="ml-6 mb-4"><a href="https://twitter.com/getgosocialapp" target="__new">Twitter</a></li>
           
           <li className="ml-6 mb-4"><a href="/privacy-policy.html" target="__new">Privacy Policy</a></li>
+          <li className="ml-6 mb-4"><a href="/terms.html" target="__new">Terms</a></li>
           
           <li className="ml-6 mb-4"><a href="mailto:gosocial@hapramp.com">gosocial@hapramp.com</a></li>
         </ul>
